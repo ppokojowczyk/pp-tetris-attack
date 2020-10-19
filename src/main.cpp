4258c420d594380a5b49a01471ea3925439ba972
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2020-10-19 22:01:40 +0200

    Game is now run in fullscreen.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     int winStyle = sf::Style::Fullscreen;
-    sf::RenderWindow window(sf::VideoMode(800, 600), "pp-tetris-attack");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "pp-tetris-attack", winStyle);
     window.setPosition(sf::Vector2i((sf::VideoMode::getDesktopMode().width / 2) - 400, (sf::VideoMode::getDesktopMode().height / 2) - 300));
     Engine engine(window);
     engine.run();
